1b933d75b0ecedd37e81729ccca4e88bd667c1ae
g3003657-sketch <g3003657@gmail.com>
2026-07-06 21:08:59 -0500

    added one joker

--- main.lua
+++ main.lua
@@ -4,24 +4,45 @@ SMODS.current_mod.init = function()
     print("Hex initialized")
 end
 
-SMODS.current_mod.keybind = function()
-    if love.keyboard.isDown("h") then
-        print("Hex key pressed!")
-    end
-end
+-- Atlas for your joker sprite sheet.
+-- Put jokers.png in YourMod/assets/1x/jokers.png (71x95 px per frame)
+SMODS.Atlas{
+    key = "HexJokers",
+    path = "jokers.png",
+    px = 71,
+    py = 95,
+}
 
-local hex_triggered = false
+SMODS.Joker{
+    key = "hex_joker",
+    loc_txt = {
+        name = "Hex Joker",
+        text = {
+            "{X:mult,C:white}X#1#{} Mult"
+        }
+    },
+    config = { extra = { Xmult = 5 } },
+    atlas = "HexJokers",
+    pos = { x = 0, y = 0 }, -- first frame in the atlas
+    rarity = 3,             -- 1 common, 2 uncommon, 3 rare, 4 legendary
+    cost = 10,
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = true,
 
-local old_update = Game.update
-function Game:update(dt)
-    old_update(self, dt)
+    -- Fires during the main scoring pass
+    calculate = function(self, card, context)
+        if context.joker_main then
+            return {
+                Xmult = card.ability.extra.Xmult,
+                message = "X" .. card.ability.extra.Xmult .. " Mult!",
+            }
+        end
+    end,
 
-    if love.keyboard.isDown("h") and not hex_triggered then
-        hex_triggered = true
-        print("Hex triggered")
-    end
-
-    if not love.keyboard.isDown("h") then
-        hex_triggered = false
-    end
-end
+    -- Fills the #1# placeholder in the description text
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.Xmult } }
+    end,
+}
