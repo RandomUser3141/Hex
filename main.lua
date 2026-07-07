454a8dd0977e417667c154942cd3be3de04d541c
g3003657-sketch <g3003657@gmail.com>
2026-07-06 21:52:09 -0500

    764

--- main.lua
+++ main.lua
@@ -14,34 +14,45 @@ SMODS.Atlas{
 }
 
 SMODS.Joker{
-    key = "hex_joker",
+    key = "green_screen",
     loc_txt = {
-        name = "Hex Joker",
+        name = "Green Screen",
         text = {
-            "{X:mult,C:white}X#1#{} Mult"
+            "This Joker gains {X:mult,C:white}X1{} Mult",
+            "every time you play a",
+            "{C:attention}Full House{}",
+            "(Currently {X:mult,C:white}X#1#{} Mult)"
         }
     },
-    config = { extra = { Xmult = 5 } },
+    config = { extra = { Xmult = 1, Xmult_gain = 1 } },
     atlas = "HexJokers",
     pos = { x = 0, y = 0 }, -- first frame in the atlas
-    rarity = 3,             -- 1 common, 2 uncommon, 3 rare, 4 legendary
-    cost = 10,
+    rarity = 4,             -- 1 common, 2 uncommon, 3 rare, 4 legendary
+    cost = 20,
     unlocked = true,
     discovered = true,
     blueprint_compat = true,
     eternal_compat = true,
 
-    -- Fires during the main scoring pass
     calculate = function(self, card, context)
+        -- Apply the current Xmult when this joker scores
         if context.joker_main then
             return {
                 Xmult = card.ability.extra.Xmult,
-                message = "X" .. card.ability.extra.Xmult .. " Mult!",
+            }
+        end
+
+        -- Grow permanently whenever a Full House is played
+        if context.before and next(context.poker_hands["Full House"]) and not context.blueprint then
+            card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_gain
+            return {
+                message = localize("k_upgrade_ex"),
+                colour = G.C.MULT,
             }
         end
     end,
 
-    -- Fills the #1# placeholder in the description text
+    -- Fills the #1# placeholder in the description text with the current Xmult
     loc_vars = function(self, info_queue, card)
         return { vars = { card.ability.extra.Xmult } }
     end,
